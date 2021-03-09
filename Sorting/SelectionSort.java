e74c4f50aa86e84cf4c62b3a594426c124b54423
John Earl <jtearl188@gmail.com>
2021-03-09 09:09:57 -0500

    solution

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class SelectionSort {
+    public static int[] selectionSort(int[] array){
+        if(array.length == 0){
+            return new int[] {};
+        }
+
+        int startIndex = 0;
+        while (startIndex < array.length - 1) {
+            int smallestPointer = startIndex;
+            for (int i = startIndex + 1; i < array.length; i++) {
+                if(array[smallestPointer] > array[i]){
+                    smallestPointer = i;
+                }
+            }
+            swap(startIndex, smallestPointer, array);
+            startIndex++;
+        }
+        return array;
+    }
+
+    public static void swap(int startIndex, int smallestPointer, int[] array){
+        int temp = array[smallestPointer];
+        array[smallestPointer] = array[startIndex];
+        array[startIndex] = temp;
+    }
+}
