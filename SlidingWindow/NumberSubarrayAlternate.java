4569747c2d7c6bf20b90d8b679001c217d03d5a9
JT <jtearl188@gmail.com>
2021-09-30 21:35:38 -0400

    replace 0s with 1s better solution

---
+++ SlidingWindow/NumberSubarrayAlternate.java
@@ -0,0 +1,23 @@
+public class NumberSubarrayAlternate {
+    public static int NumberSubarray(int[] array, int replacementCount) {
+        int windowStart = 0, result = 0, maxOneCount = 0;
+        for (int windowEnd = 0; windowEnd < array.length; windowEnd++) {
+            if(array[windowEnd] == 1){
+                maxOneCount++;
+            }
+
+            if (windowEnd - windowStart + 1 - maxOneCount > replacementCount) {
+                if (array[windowStart] == 1) {
+                    maxOneCount--;
+                }
+                windowStart++;
+            }
+            result = Math.max(result, windowEnd - windowStart + 1);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(NumberSubarray(new int[] { 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1 }, 2));
+        System.out.println(NumberSubarray(new int[] { 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1 }, 3));
+    }
+}
