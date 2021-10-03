8a49c9040adf146b5261ae0dbd4cad91a36c6f6e
JT <jtearl188@gmail.com>
2021-10-03 11:31:20 -0400

    adding solution

---
+++ TwoPointer/RemoveDupes.java
@@ -0,0 +1,19 @@
+public class RemoveDupes {
+    public static int removeDuplicates(int[] array) {
+        int nextNonDuplicate = 1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[nextNonDuplicate - 1] != array[i]){
+                array[nextNonDuplicate] = array[i];
+                nextNonDuplicate++;
+            }       
+        }
+        return nextNonDuplicate;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[] { 2, 3, 3, 3, 6, 9, 9 };
+        System.out.println(removeDuplicates(arr));
+    
+        arr = new int[] { 2, 2, 2, 11 };
+        System.out.println(removeDuplicates(arr));
+    }
+}
