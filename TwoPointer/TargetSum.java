face1db72a17f394e2aeecc8101b6e7fc9f1f2d5
JT <jtearl188@gmail.com>
2021-10-03 10:54:15 -0400

    adding map solution

--- TwoPointer/TargetSum.java
+++ TwoPointer/TargetSum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TargetSum {
     public static int[] twoSum(int[] array, int target) {
         int pointer1 = 0, pointer2 = array.length-1, sum = Integer.MIN_VALUE;
@@ -17,10 +20,27 @@ public class TargetSum {
         return result;
     }
 
+    public static int[] twoSumMap(int[] array, int target) {
+        Map<Integer, Integer> nums = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if(nums.containsKey(target - array[i])){
+                return new int[] {nums.get(target - array[i]), i};
+            }else{
+                nums.put(array[i], i);
+            }
+        }
+        return new int[] {-1, -1}; //not found
+    }
+
     public static void main(String[] args) {
         int[] result = twoSum(new int[] { 1, 2, 3, 4, 6 }, 6);
         System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
         result = twoSum(new int[] { 2, 5, 9, 11 }, 11);
         System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+
+        result = twoSumMap(new int[] { 1, 2, 3, 4, 6 }, 6);
+        System.out.println("Map: [" + result[0] + ", " + result[1] + "]");
+        result = twoSumMap(new int[] { 2, 5, 9, 11 }, 11);
+        System.out.println("Map: [" + result[0] + ", " + result[1] + "]");
   }
 }
