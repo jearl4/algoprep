bcf659a5e287057ef5fe277be9157db5d4e70e01
JT <jtearl188@gmail.com>
2021-10-04 20:13:59 -0400

    added inline solution

--- TwoPointer/TripletSmallerSum.java
+++ TwoPointer/TripletSmallerSum.java
@@ -1,7 +1,29 @@
 import java.util.Arrays;
 
 public class TripletSmallerSum {
-    public static int smallerSums(int[] array, int target) {
+    public static int smallerSumsInline(int[] array, int target) {
+        Arrays.sort(array);
+        int count = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            // count += searchPair(array, target - array[i], i); 
+            int tempCount = 0,
+                pointerA = i + 1,
+                pointerB = array.length - 1;
+            while (pointerA < pointerB) {
+                int difference = array[pointerA] + array[pointerB];
+                if (difference < target - array[i]) {
+                    tempCount += pointerB - pointerA;
+                    pointerA++;
+                }else{
+                    pointerB--;
+                }
+            }
+            count += tempCount;
+        }
+        return count;
+    }
+
+    public static int smallerSumsRefactor(int[] array, int target) {
         Arrays.sort(array);
         int count = 0;
         for (int i = 0; i < array.length - 2; i++) {
@@ -27,7 +49,9 @@ public class TripletSmallerSum {
     }
 
     public static void main(String[] args) {
-        System.out.println(smallerSums(new int[] { -1, 0, 2, 3 }, 3));
-        System.out.println(smallerSums(new int[] { -1, 4, 2, 1, 3 }, 5));
+        System.out.println(smallerSumsRefactor(new int[] { -1, 0, 2, 3 }, 3));
+        System.out.println(smallerSumsRefactor(new int[] { -1, 4, 2, 1, 3 }, 5));
+        System.out.println(smallerSumsInline(new int[] { -1, 0, 2, 3 }, 3));
+        System.out.println(smallerSumsInline(new int[] { -1, 4, 2, 1, 3 }, 5));
     }
 }
