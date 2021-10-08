05d0fce4012b094f0a6c49356cf2ee4e601d1cb5
JT <jtearl188@gmail.com>
2021-10-08 11:29:54 -0400

    linked list dto class and cycle length solution

--- Fast&SlowPointers/LinkedListCycleLength.java
+++ Fast&SlowPointers/LinkedListCycleLength.java
@@ -1,14 +1,24 @@
-public class LinkedListCycle {
-    public static boolean hasCycle(ListNodeDTO head) {
+public class LinkedListCycleLength {
+    public static int findCycleLength(ListNodeDTO head) {
         ListNodeDTO slow = head, fast = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (slow == fast) {
-                return true;
+            if(slow == fast){
+                return calculateLength(slow);
             }
         }
-        return false;
+        return 0;
+    }
+
+    private static int calculateLength(ListNodeDTO slow) {
+        ListNodeDTO current = slow;
+        int cycleLength = 0;
+        do {
+            current = current.next;
+            cycleLength++;
+        } while (current != slow);
+        return cycleLength;
     }
 
     public static void main(String[] args) {
@@ -18,12 +28,10 @@ public class LinkedListCycle {
         head.next.next.next = new ListNodeDTO(4);
         head.next.next.next.next = new ListNodeDTO(5);
         head.next.next.next.next.next = new ListNodeDTO(6);
-        System.out.println("LinkedList has cycle: " + hasCycle(head));
-    
         head.next.next.next.next.next.next = head.next.next;
-        System.out.println("LinkedList has cycle: " + hasCycle(head));
+        System.out.println("LinkedList cycle length: " + LinkedListCycleLength.findCycleLength(head));
     
         head.next.next.next.next.next.next = head.next.next.next;
-        System.out.println("LinkedList has cycle: " + hasCycle(head));
+        System.out.println("LinkedList cycle length: " + LinkedListCycleLength.findCycleLength(head));
       }
 }
---
+++ Fast&SlowPointers/ListNodeDTO.java
@@ -0,0 +1,8 @@
+public class ListNodeDTO {
+    int value = 0;
+    ListNodeDTO next;
+    
+    ListNodeDTO(int value) {
+        this.value = value;
+    }
+}
