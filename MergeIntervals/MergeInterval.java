34becce3ee6e474d927c6e4abb97bf079826cd2d
JT <jtearl188@gmail.com>
2021-10-09 13:36:32 -0400

    solution

--- MergeIntervals/MergeInterval.java
+++ MergeIntervals/MergeInterval.java
@@ -3,7 +3,29 @@ import DTO.*;
 
 public class MergeInterval {
     public static List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() < 2) {
+            return intervals;
+        }
+
+        Collections.sort(intervals, (a,b) -> Integer.compare(a.start, b.start));
+
         List<Interval> mergedIntervals = new LinkedList<Interval>();
+        Iterator<Interval> intervalItr = intervals.iterator();
+        Interval interval = intervalItr.next();
+        int start = interval.start;
+        int end = interval.end;
+
+        while (intervalItr.hasNext()) {
+            interval = intervalItr.next();
+            if (interval.start <= end) {
+                end = Math.max(interval.end, end);
+            }else{
+                mergedIntervals.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+        mergedIntervals.add(new Interval(start, end));
         return mergedIntervals;
     }
 
