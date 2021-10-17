4141fe8eb1c4036444a7beb59d8a2fb2c89c6c49
John Earl <jtearl188@gmail.com>
2021-10-17 10:36:26 -0400

    solution

---
+++ tree/DepthFirstSearch/AllPathsForSum.java
@@ -0,0 +1,40 @@
+package Tree.DepthFirstSearch;
+import java.util.*;
+import Tree.DTO.TreeNode;
+
+public class AllPathsForSum {
+    public static List<List<Integer>> findAllPaths(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        findPathsRecursive(root, sum, path, result);
+        return result;
+    }
+
+    private static void findPathsRecursive(TreeNode currentNode, int sum, List<Integer> currentPath, List<List<Integer>> allPaths) {
+        if (currentNode == null) {
+            return;
+        }
+
+        currentPath.add(currentNode.val);
+
+        if (currentNode.val == sum && currentNode.left == null && currentNode.right == null) {
+            allPaths.add(new ArrayList<Integer>(currentPath));
+        } else{
+            findPathsRecursive(currentNode.left, sum - currentNode.val, currentPath, allPaths);
+            findPathsRecursive(currentNode.right, sum - currentNode.val, currentPath, allPaths);
+        }
+        currentPath.remove(currentPath.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        int sum = 23;
+        List<List<Integer>> result = findAllPaths(root, sum);
+        System.out.println("Tree paths with sum " + sum + ": " + result);
+    }
+}
