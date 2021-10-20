69d01edebb390a09917e338fd81ac2c819a8ddc4
JT <jtearl188@gmail.com>
2021-10-20 18:03:49 -0400

    solution

---
+++ TwoHeaps/DTO/Interval.java
@@ -0,0 +1,11 @@
+package TwoHeaps.DTO;
+
+public class Interval {
+    public int start = 0;
+    public int end = 0;
+  
+    public Interval(int start, int end) {
+      this.start = start;
+      this.end = end;
+    }
+  }
