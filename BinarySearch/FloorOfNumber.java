94cb3cbd08fc928915295ac973836b842858276b
JT <jtearl188@gmail.com>
2021-10-22 14:30:33 -0400

    solution

---
+++ BinarySearch/FloorOfNumber.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class FloorOfNumber {
+    public static int searchFloorOfANumber(int[] arr, int key) {
+        if (key < arr[0]) {
+            return -1;
+        }
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            if(key == arr[middle]){
+                return middle;
+            }else if(key < arr[middle]){
+                end = middle - 1;
+            }else if(key > arr[middle]){
+                start = middle + 1;
+            }
+        }
+        return end;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchFloorOfANumber(new int[] { 4, 6, 10 }, 6));
+        System.out.println(searchFloorOfANumber(new int[] { 1, 3, 8, 10, 15 }, 12));
+        System.out.println(searchFloorOfANumber(new int[] { 4, 6, 10 }, 17));
+        System.out.println(searchFloorOfANumber(new int[] { 4, 6, 10 }, -1));
+    }
+}
