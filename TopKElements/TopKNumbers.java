34d5b64cb78225baf5f779aac5fb60dfa0b66dbe
JT <jtearl188@gmail.com>
2021-10-27 19:49:33 -0400

    solution

---
+++ TopKElements/TopKNumbers.java
@@ -0,0 +1,32 @@
+package TopKElements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class TopKNumbers {
+    public static List<Integer> findKLargestNumbers(int[] nums, int k){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        for (int i = k; i < nums.length; i++) {
+            minHeap.add(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(nums[i]);
+            }
+        }
+
+        return new ArrayList<>(minHeap);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    
+        result = findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
+}
