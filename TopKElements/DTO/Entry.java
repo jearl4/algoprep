c389d747eea9d3ab4ce17c8ab73571e770f33933
JT <jtearl188@gmail.com>
2021-10-28 17:25:39 -0400

    solution

---
+++ TopKElements/DTO/Entry.java
@@ -0,0 +1,11 @@
+package TopKElements.DTO;
+
+public class Entry {
+    public int key;
+    public int value;
+
+    public Entry(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
