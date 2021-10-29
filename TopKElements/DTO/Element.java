c5c60bd4a07970b8a6095622128c975a07283100
JT <jtearl188@gmail.com>
2021-10-29 18:41:01 -0400

    solution

---
+++ TopKElements/DTO/Element.java
@@ -0,0 +1,13 @@
+package TopKElements.DTO;
+
+public class Element {
+    public int number;
+    public int frequency;
+    public int sequenceNumber;
+  
+    public Element(int number, int frequency, int sequenceNumber) {
+      this.number = number;
+      this.frequency = frequency;
+      this.sequenceNumber = sequenceNumber;
+    }
+}
