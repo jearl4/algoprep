8ddb7e68ca1816cf96bed554ec6410686648c39d
JT <jtearl188@gmail.com>
2021-10-30 11:42:10 -0400

    solution

---
+++ KWayMerge/DTO/Node.java
@@ -0,0 +1,11 @@
+package KWayMerge.DTO;
+
+public class Node {
+    public int elementIndex;
+    public int arrayIndex;
+  
+    public Node(int elementIndex, int arrayIndex) {
+      this.elementIndex = elementIndex;
+      this.arrayIndex = arrayIndex;
+    }
+}
