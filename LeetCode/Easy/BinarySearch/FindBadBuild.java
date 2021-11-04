e8a3eeedfb0611fc48cb9532fa229b17ea8543f8
JT <jtearl188@gmail.com>
2021-11-03 20:36:10 -0400

    solution

--- LeetCode/Easy/BinarySearch/FindBadBuild.java
+++ LeetCode/Easy/BinarySearch/FindBadBuild.java
@@ -5,16 +5,15 @@ package LeetCode.Easy.BinarySearch;
 
 public class FindBadBuild {
     public int firstBadVersion(int n) {
-        int start = 0, end = n, badVersionIndex = Integer.MAX_VALUE;
-        while (start <= end) {
+        int start = 0, end = n;
+        while(start <= end){
             int mid = start + (end - start) / 2;
-            if (isBadVersion(mid)) {
+            if(isBadVersion(mid)){
                 end = mid - 1;
-                badVersionIndex = Math.min(mid, badVersionIndex);
-            } else if (!isBadVersion(mid)) {
+            }else{
                 start = mid + 1;
             }
         }
-        return badVersionIndex;
+        return start;
     }
 }
