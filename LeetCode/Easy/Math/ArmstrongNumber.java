0a2c4c66501a8c6df403e5b16f779a15737e53be
JT <jtearl188@gmail.com>
2021-11-06 13:09:52 -0400

    solution

---
+++ LeetCode/Easy/Math/ArmstrongNumber.java
@@ -0,0 +1,19 @@
+package LeetCode.Easy.Math;
+
+public class ArmstrongNumber {
+    public boolean isArmstrong(int n) {
+        int sum = 0;
+        int nClone = n;
+        int power = Integer.toString(n).length();
+        for(int i = 0; i < power; i++){
+            int digit = nClone % 10;
+            sum += Math.pow(digit, power);
+            nClone /= 10;
+        }
+        
+        if(sum == n)
+            return true;
+        return false;
+    }
+    }
+}
