00b94f93c7d839c5649cd25798cf350bddb90163
JT <jtearl188@gmail.com>
2021-11-06 12:21:27 -0400

    solution

--- LeetCode/Easy/BinarySearch/IntersectionThreeSortedArrays.java
+++ LeetCode/Easy/BinarySearch/IntersectionThreeSortedArrays.java
@@ -2,26 +2,25 @@ package LeetCode.Easy.BinarySearch;
 
 public class IntersectionThreeSortedArrays {
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
-        Map<Integer, Integer> map = new LinkedHashMap();
-        List<Integer> solution = new ArrayList();
-
-        for (Integer num : arr1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        for (Integer num : arr2) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        for (Integer num : arr3) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-        for (Integer item : map.keySet()) {
-            if (map.get(item) == 3) {
-                solution.add(item);
+        public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+            List<Integer> solution = new ArrayList();
+            int p1 = 0, p2 = 0, p3 = 0;
+            while(p1 < arr1.length && p2 < arr2.length && p3 < arr3.length){
+                if(arr1[p1] == arr2[p2] && arr2[p2] == arr3[p3]){
+                    solution.add(arr1[p1]);
+                    p1++;
+                    p2++;
+                    p3++;
+                }else if(arr1[p1] < arr2[p2]){
+                    p1++;
+                }else if(arr2[p2] < arr3[p3]){
+                    p2++;
+                }else{
+                    p3++;
+                }
             }
+            
+            return solution;
         }
-        return solution;
     }
 }
