5b6580cc67c34c63270dfc651e526c5a3b9e5973
JT <jtearl188@gmail.com>
2021-11-07 10:48:42 -0500

    solution

--- LeetCode/Easy/HashTable/ContainsDuplicate.java
+++ LeetCode/Easy/HashTable/ContainsDuplicate.java
@@ -1,8 +1,11 @@
 package LeetCode.Easy.HashTable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet();
+        Set<Integer> set = new HashSet<Integer>();
         for (Integer num : nums) {
             if (set.contains(num)) {
                 return true;
