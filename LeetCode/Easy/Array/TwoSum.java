200a60715f4014aac979c177719f8e45393b360a
JT <jtearl188@gmail.com>
2021-11-10 09:04:54 -0500

    optimized

--- LeetCode/Easy/Array/TwoSum.java
+++ LeetCode/Easy/Array/TwoSum.java
@@ -1,12 +1,18 @@
 package LeetCode.Easy.Array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[] {i, j};
-                }
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) {
+                return new int[] { i, map.get(target - nums[i]) };
             }
         }
         return null;
