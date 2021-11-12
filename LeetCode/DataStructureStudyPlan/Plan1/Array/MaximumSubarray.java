55d3858c7716a86d1651ca981658c4d7c19e3aa2
JT <jtearl188@gmail.com>
2021-11-12 09:19:44 -0500

    solution

--- LeetCode/DataStructureStudyPlan/Plan1/Array/MaximumSubarray.java
+++ LeetCode/DataStructureStudyPlan/Plan1/Array/MaximumSubarray.java
@@ -2,13 +2,11 @@ package LeetCode.DataStructureStudyPlan.Plan1.Array;
 
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int maxSubarray = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int currentSubarray = 0;
-            for (int j = i; j < nums.length; j++) {
-                currentSubarray += nums[j];
-                maxSubarray = Math.max(maxSubarray, currentSubarray);
-            }
+        int currentSubarray = nums[0], maxSubarray = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
         }
         return maxSubarray;
     }
