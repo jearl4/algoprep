d8fe2f336f5e64b1f841be466da3f7301093b685
JT <jtearl188@gmail.com>
2021-11-12 17:59:13 -0500

    solution

---
+++ LeetCode/General/Array/BuildArrayFromPermutation.java
@@ -0,0 +1,13 @@
+package LeetCode.General.Array;
+
+public class BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int[] solution = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            solution[i] = nums[nums[i]];
+        }
+
+        return solution;
+    }
+}
