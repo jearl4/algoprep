1df7f0d1ee784691ee06e308871318159ce9318e
JT <jtearl188@gmail.com>
2021-11-13 13:06:35 -0500

    solution

--- LeetCode/General/String/ValueOfVariableAfterOperations.java
+++ LeetCode/General/String/ValueOfVariableAfterOperations.java
@@ -1,15 +1,11 @@
 package LeetCode.General.String;
 
 public class ValueOfVariableAfterOperations {
-    public int finalValueAfterOperations(String[] operations) {
-        int sum = 0;
-
-        for (String s : operations) {
-            if (s.equals("++X") || s.equals("X++")) {
-                sum++;
-            } else {
-                sum--;
-            }
+    public int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
         }
         return sum;
     }
