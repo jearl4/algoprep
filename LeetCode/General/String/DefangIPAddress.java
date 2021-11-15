37f4541fba551eae44d08082a58cbc3724db24d9
JT <jtearl188@gmail.com>
2021-11-15 10:51:46 -0500

    solution

--- LeetCode/General/String/DefangIPAddress.java
+++ LeetCode/General/String/DefangIPAddress.java
@@ -1,4 +1,17 @@
 package LeetCode.General.String;
 
 public class DefangIPAddress {
+    public String defangIPaddr(String address) {
+        StringBuilder str = new StringBuilder();
+        int end = address.length();
+        for (int i = 0; i < end; i++){
+            char curr = address.charAt(i);
+            if (curr == '.'){
+                str.append("[.]");
+            } else {
+                str.append(curr);
+            }
+        }
+        return str.toString();
+    }
 }
