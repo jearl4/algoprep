1b668bce5ad5495a88ae5aaefb4304df1209a3fd
JT <jtearl188@gmail.com>
2021-11-15 13:53:24 -0500

    in place solution

--- LeetCode/General/Array/RichestCustomer.java
+++ LeetCode/General/Array/RichestCustomer.java
@@ -1,26 +1,17 @@
 package LeetCode.General.Array;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RichestCustomer {
     public int maximumWealth(int[][] accounts) {
         int maxWealth = Integer.MIN_VALUE;
-        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < accounts.length; i++) {
             int wealth = 0;
             for (int j = 0; j < accounts[i].length; j++) {
                 wealth += accounts[i][j];
             }
-            map.put(i, wealth);
+            maxWealth = Math.max(maxWealth, wealth);
         }
 
-        for (int i = 0; i < map.size(); i++) {
-            if (map.get(i) > maxWealth) {
-                maxWealth = map.get(i);
-            }
-        }
         return maxWealth;
     }
 }
