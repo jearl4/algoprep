755ba74e86a6334b7efcee837711ea3913e6841c
JT <jtearl188@gmail.com>
2021-11-15 15:37:20 -0500

    solution

--- LeetCode/General/Array/GreatestCandy.java
+++ LeetCode/General/Array/GreatestCandy.java
@@ -6,17 +6,16 @@ import java.util.List;
 public class GreatestCandy {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> solution = new ArrayList<>();
-
-        for (int i = 0; i < candies.length; i++) {
-            boolean isMostCandy = true;
-            int totalCandy = candies[i] + extraCandies;
-            for (int j = 0; j < candies.length; j++) {
-                if (totalCandy < candies[j]) {
-                    isMostCandy = false;
-                }
-            }
-            solution.add(isMostCandy);
+        int maxCandies = 0;
+        
+        for(int i = 0; i < candies.length; i++){
+            maxCandies = Math.max(maxCandies, candies[i]);
         }
+        
+        for(int i = 0; i < candies.length; i++){
+            solution.add(candies[i] + extraCandies >= maxCandies);
+        }
+         
         return solution;
-    }
+        }
 }
