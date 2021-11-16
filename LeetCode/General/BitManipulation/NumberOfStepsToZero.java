fd98e8d5afe00067082255fc8479c32709ad94a3
JT <jtearl188@gmail.com>
2021-11-16 08:51:02 -0500

    brute force solution

---
+++ LeetCode/General/BitManipulation/NumberOfStepsToZero.java
@@ -0,0 +1,16 @@
+package LeetCode.General.BitManipulation;
+
+public class NumberOfStepsToZero {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num != 0) {
+            if (num % 2 != 0) {
+                num -= 1;
+            } else {
+                num /= 2;
+            }
+            count++;
+        }
+        return count;
+    }
+}
