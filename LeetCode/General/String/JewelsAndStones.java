906ece0a84086a0bb0f055adb4566c37938045bd
JT <jtearl188@gmail.com>
2021-11-16 08:13:38 -0500

    solution

---
+++ LeetCode/General/String/JewelsAndStones.java
@@ -0,0 +1,16 @@
+package LeetCode.General.String;
+
+public class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+
+        for (int i = 0; i < jewels.length(); i++) {
+            for (int j = 0; j < stones.length(); j++) {
+                if (jewels.charAt(i) == stones.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
