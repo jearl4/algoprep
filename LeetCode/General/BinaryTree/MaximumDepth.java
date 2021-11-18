e3ec5a5cf1e70553beb1e8956c2413a9c56a2352
JT <jtearl188@gmail.com>
2021-11-18 09:21:30 -0500

    simplified dfs solution

--- LeetCode/General/BinaryTree/MaximumDepth.java
+++ LeetCode/General/BinaryTree/MaximumDepth.java
@@ -3,27 +3,13 @@ package LeetCode.General.BinaryTree;
 import Educative.Tree.DTO.TreeNode;
 
 public class MaximumDepth {
-    int bestSum = 0, currentSum = 0;
-
     public int maxDepth(TreeNode root) {
-        if (root == null)
+        if(root == null){
             return 0;
-
-        currentSum++;
-
-        if (root.left != null)
-            maxDepth(root.left);
-
-        if (root.right != null)
-            maxDepth(root.right);
-
-        if (root.right == null && root.left == null) {
-            if (currentSum > bestSum) {
-                bestSum = currentSum;
-            }
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
         }
-        currentSum--;
-
-        return bestSum;
     }
 }
