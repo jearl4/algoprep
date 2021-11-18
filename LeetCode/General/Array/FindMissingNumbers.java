74e8edb6814319a3aa63de17a993b6389996208e
JT <jtearl188@gmail.com>
2021-11-18 18:34:16 -0500

    in-place solution

--- LeetCode/General/Array/FindMissingNumbers.java
+++ LeetCode/General/Array/FindMissingNumbers.java
@@ -1,21 +1,20 @@
 package LeetCode.General.Array;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class FindMissingNumbers {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Set<Integer> set = new HashSet<>();
         List<Integer> solution = new ArrayList<>();
-        
-        for(int num : nums){
-            set.add(num);
+
+        for (int i = 0; i < nums.length; i++) {
+            int newIndex = Math.abs(nums[i]) - 1;
+            if (nums[newIndex] > 0)
+                nums[newIndex] *= -1;
         }
-        
-        for(int i = 1; i <= nums.length; i++){
-            if(!set.contains(i)){
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (nums[i - 1] > 0) {
                 solution.add(i);
             }
         }
