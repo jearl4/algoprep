bddcc3edef096660778ec68827348069367be661
JT <jtearl188@gmail.com>
2021-11-20 14:19:01 -0500

    solution

--- LeetCode/General/String/ToLowerCase.java
+++ LeetCode/General/String/ToLowerCase.java
@@ -1,8 +1,22 @@
 package LeetCode.General.String;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ToLowerCase {
     public String toLowerCase(String s) {
-        s = s.toLowerCase();
-        return s;
+        Map<Character, Character> map = new HashMap<>();
+        String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < 26; i++) {
+            map.put(upperCase.charAt(i), lowerCase.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append(map.containsKey(c) ? map.get(c) : c);
+        }
+        return sb.toString();
     }
 }
