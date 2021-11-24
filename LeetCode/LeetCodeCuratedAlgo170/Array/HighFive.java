9ff415d98f27555a4a9fe6cbbf55456d2b8c5c24
JT <jtearl188@gmail.com>
2021-11-24 16:48:52 -0500

    no solution

--- LeetCode/LeetCodeCuratedAlgo170/Array/HighFive.java
+++ LeetCode/LeetCodeCuratedAlgo170/Array/HighFive.java
@@ -1,20 +1,10 @@
 package LeetCode.LeetCodeCuratedAlgo170.Array;
 
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HighFive {
     public int[][] highFive(int[][] items) {
-        Stack<Integer> stack = new Stack<>((n1, n2) -> n2 - n1);
-        int sum = 0;
-        
-        for(int i = 0; i < items.length; i++){
-            stack.push(items[i][1]);
-        }
-        
-        for(int i = 0; i < 5; i++){
-            sum += stack.pop();
-        }
-        
-        return sum / 5;
+     
     }
 }
