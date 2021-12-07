efb302fdd13e2192725ccda56c4b42946832f8f5
JT <jtearl188@gmail.com>
2021-12-06 21:05:02 -0500

    solution

--- LeetCode/General/String/DistinctSubstrings1876.java
+++ LeetCode/General/String/DistinctSubstrings1876.java
@@ -8,17 +8,17 @@ public class DistinctSubstrings1876 {
         HashMap<Character, Integer> map = new HashMap<>();
 
         for(int end = 0; end < s.length(); end++){
-            char c = s.charAt(end);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            char endCharacter = s.charAt(end);
+            map.put(endCharacter, map.getOrDefault(endCharacter, 0) + 1);
             if((end - start + 1) == 3){
                 if(map.size() == 3){
                     goodStrings++;
                 }
 
-                char c2 = s.charAt(start++);
-                map.put(c2, map.get(c2) - 1);
-                if(map.get(c2) == 0){
-                    map.remove(c2);
+                char frontCharacter = s.charAt(start++);
+                map.put(frontCharacter, map.get(frontCharacter) - 1);
+                if(map.get(frontCharacter) == 0){
+                    map.remove(frontCharacter);
                 }
             }
         }
