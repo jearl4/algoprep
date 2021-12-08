3acc098f575a0ad5a9ff48636027d034ca64f114
JT <jtearl188@gmail.com>
2021-12-07 20:44:38 -0500

    solution

---
+++ LeetCode/Explore/HashTable/IntersectionTwoArrays349.java
@@ -0,0 +1,61 @@
+package LeetCode.Explore.HashTable;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class IntersectionTwoArrays349 {
+
+    // using java prebuilt retainAll method
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (Integer num : nums1) {
+            set1.add(num);
+        }
+
+        for (Integer num : nums2) {
+            set2.add(num);
+        }
+
+        set1.retainAll(set2);
+
+        int[] output = new int[set1.size()];
+        int i = 0;
+        for (int s : set1) {
+            output[i++] = s;
+        }
+        return output;
+    }
+
+    // using custom intersection function
+    public int[] customIntersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (Integer num : nums1) {
+            set1.add(num);
+        }
+
+        for (Integer num : nums2) {
+            set2.add(num);
+        }
+
+        if(set1.size() > set2.size()){
+            return setIntersection(set1, set2);
+        }else{
+            return setIntersection(set2, set1);
+        }
+    }
+
+    private int[] setIntersection(HashSet<Integer> set1, HashSet<Integer> set2) {
+        int[] output = new int[set1.size()];
+        int i = 0;
+        for(Integer num : set1){
+            if(set2.contains(num)){
+                output[i++] = num;
+            }
+        }
+        return Arrays.copyOf(output, i);
+    }
+}
