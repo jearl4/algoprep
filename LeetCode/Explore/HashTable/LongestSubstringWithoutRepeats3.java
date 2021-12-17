380ad27103297405e3b94e7926f6437613e6613b
JT <jtearl188@gmail.com>
2021-12-17 15:03:46 -0500

    solution

---
+++ LeetCode/Explore/HashTable/LongestSubstringWithoutRepeats3.java
@@ -0,0 +1,24 @@
+package LeetCode.Explore.HashTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeats3 {
+    public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+        int start = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (map.get(c) >= start) {
+                    start = map.get(c) + 1;
+                }
+            }
+            length = Math.max(length, i - start + 1);
+            map.put(c, i);
+        }
+        return length;
+    }
+}
