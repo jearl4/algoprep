4648e14a98f4c397b775c5c754ce284b416af316
JT <jtearl188@gmail.com>
2021-12-18 10:39:23 -0500

    solution

---
+++ LeetCode/Explore/HashTable/TwoSumDataStructure170.java
@@ -0,0 +1,32 @@
+package LeetCode.Explore.HashTable;
+
+public class TwoSumDataStructure170 {
+    private HashMap<Integer, Integer> map;
+    
+    public TwoSum() {
+        this.map = new HashMap<Integer, Integer>();
+    }
+    
+    public void add(int number) {
+        this.map.put(number, map.getOrDefault(number, 0) + 1);
+    }
+    
+    public boolean find(int value) {
+        for(Integer num : map.keySet()){
+            int complement = value - num;
+            if(map.containsKey(complement)){
+                if (num != complement || map.get(complement) > 1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
