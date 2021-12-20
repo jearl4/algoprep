f9a0da940e0349b0bf05cc6e5624fb0981212e3f
JT <jtearl188@gmail.com>
2021-12-20 08:53:40 -0500

    solution

---
+++ LeetCode/Explore/HashTable/FourSumII454.java
@@ -0,0 +1,22 @@
+package LeetCode.Explore.HashTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumII454 {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                map.put(num1 + num2, map.getOrDefault(num1 + num2, 0) + 1);
+            }
+        }
+        for (int num3 : nums3) {
+            for (int num4 : nums4) {
+                count += map.getOrDefault(-(num3 + num4), 0);
+            }
+        }
+        return count;
+    }
+}
